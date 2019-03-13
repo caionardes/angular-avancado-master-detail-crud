908cb8db2420e79ab23338c50b17f5010476d423
caionardesgft <caio.nardes@gft.com>
2019-03-13 17:43:14 -0300

    Tratamento de erro do backend no crud de categoria.

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -78,7 +78,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
     if (error.status === 422) {
       this.serverErrorMessages = JSON.parse(error._body).errors;
     } else {
-      this.serverErrorMessages = ['Falha na comunicação com servidor!'];
+      this.serverErrorMessages = ['Falha na comunicação com servidor. Por favor, tente mais tarde.'];
     }
   }
 
