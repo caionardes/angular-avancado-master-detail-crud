58c7e3f926c63545f286c1bce37b9f2c1bd4914b
caionardesgft <caio.nardes@gft.com>
2019-03-14 17:45:54 -0300

    Reaproveitando codigo que preenche 'entry.category' nos metodos create/update

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -4,7 +4,7 @@ import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { Entry } from './entry.model';
 import { Observable } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,25 +14,25 @@ export class EntryService extends BaseResourceService<Entry> {
   constructor(
       protected injector: Injector,
       private categoryService: CategoryService
-    ) {
-      super('api/entries', injector, Entry.fromJson);
-    }
+  ) {
+    super('api/entries', injector, Entry.fromJson);
+  }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.create(entry);
-      })
-    );
+    return this.fillCategoryBeforeSend(entry, super.create.bind(this));
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.fillCategoryBeforeSend(entry, super.update.bind(this));
+  }
+
+  private fillCategoryBeforeSend(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
-        return super.update(entry);
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
     );
   }
 }
