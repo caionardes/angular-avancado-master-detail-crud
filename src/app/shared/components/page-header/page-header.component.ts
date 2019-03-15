f4cbb0fc93bbc4cf52c1a2554f09d50080958591
caionardesgft <caio.nardes@gft.com>
2019-03-15 11:04:23 -0300

    Criado componente Page-Header.

---
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-page-header',
+  templateUrl: './page-header.component.html',
+  styleUrls: ['./page-header.component.css']
+})
+export class PageHeaderComponent implements OnInit {
+
+  // tslint:disable-next-line:no-input-rename
+  @Input('page-title') pageTitle;
+  // tslint:disable-next-line:no-input-rename
+  @Input('button-class') buttonClass;
+  // tslint:disable-next-line:no-input-rename
+  @Input('button-text') buttonText;
+  // tslint:disable-next-line:no-input-rename
+  @Input('button-link') buttonLink;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
